f084e4877625da6180cd20019b1f805f00d802a0
Kyle Parry <kyle.parry.303@gmail.com>
2021-10-01 10:07:07 -0600

    Add Linkedin provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -24,6 +24,11 @@ const options = {
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
       domain: process.env.AUTH0_DOMAIN,
     }),
+    Providers.Linkedin({
+      clientId: process.env.LINKEDIN_ID,
+      clientSecret: process.env.LINKEDIN_SECRET,
+      domain: process.env.AUTH0_DOMAIN,
+    }),
     Providers.Email({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
